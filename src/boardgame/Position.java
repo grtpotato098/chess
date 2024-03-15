0a95198656a9411c7341af424d932f9ab288310c
nelio alves <jessfreitas70@gmail.com>
2024-03-15 00:51:21 -0300

    chess

---
+++ src/boardgame/Position.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+public class Position {
+	
+	private int row;
+	private int column;
+	
+	
+	public Position(int row, int column) {
+		super();
+		this.row = row;
+		this.column = column;
+	}
+	
+	
+	
+	
+
+}
