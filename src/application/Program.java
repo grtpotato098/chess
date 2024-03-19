bff963a57fbc8e79ac45c583259c355985260a52
nelio alves <jessfreitas70@gmail.com>
2024-03-19 01:09:10 -0300

    chess layer and print the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
@@ -8,8 +9,8 @@ public class Program {
 		// TODO Auto-generated method stub
 		
 		
-		Position pos = new Position(3, 5);
-		System.out.println("pos");
+		ChessMatch chessMatch = new ChessMatch();
+		UI.printBoard(chessMatch.getPieces());
 	}
 
 }
--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,42 @@
 package boardgame;
 
 public class Board {
+	
+	private int rows;
+	private int columns;
+	private Piece[][] pieces;
+	
+	
+	public Board(int rows, int columns) {
+		
+		this.rows = rows;
+		this.columns = columns;
+		pieces = new Piece[rows][columns];
+	}
+
+
+	public int getRows() {
+		return rows;
+	}
+
+
+	
+
+
+	public int getColumns() {
+		return columns;
+	}
+	
+	public Piece piece(int row, int column) {
+		
+		return pieces[row][column];
+	}
+
+
+	
+	
+	
+	
+	
 
 }
