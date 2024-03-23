d36ce955edf6a9562eadda832ed3c89b9d23804f
nelio alves <jessfreitas70@gmail.com>
2024-03-23 16:05:56 -0300

    ChessException and ChessPosition

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -32,10 +32,15 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	
 	private void initialSetup() {
 		
-		board.placePiece(new Rook(board, Color.WHITE), new Position(2, 2));
+		placeNewPiece('b', 6, new Rook(board, Color.WHITE));
 		board.placePiece(new King(board, Color.BLACK), new Position(2, 2));
 	}
 }
