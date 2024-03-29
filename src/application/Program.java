6e7095378fc80341b3e4bae281dc42d0f70ae286
nelio alves <jessfreitas70@gmail.com>
2024-03-29 09:43:10 -0300

    moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,31 @@
 package application;
 
-import boardgame.Position;
+import java.util.Scanner;
+
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.ChessPosition;
 
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		
+		Scanner sc = new Scanner(System.in);
 		ChessMatch chessMatch = new ChessMatch();
+		
+		while(true) {
 		UI.printBoard(chessMatch.getPieces());
+		System.out.println();
+		System.out.print("source: ");
+		ChessPosition source = UI.readChessPosition(sc);
+		
+		System.out.println();
+		System.out.print("target: ");
+		ChessPosition target = UI.readChessPosition(sc);
+		
+		ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+		}
 	}
 
 }
